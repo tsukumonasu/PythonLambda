fd57e8ad976d25796f415277b218f6b2fa7c403e
hirakumogama <t.naiki1985@gmail.com>
2016-04-21 07:17:45 +0900

    abc

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-print "hoge"
-print 5*4
+# -*- coding: utf-8 -*- 
+
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+class TankInterface(object, metaclass=abc.ABCMeta):
+    
+    @abstractproperty
+        def name(self):
+            pass
+            
+
+
+# if __name__ == "__main__":
+
+#     classList    =    []
+#     classList.append(TankInterface(1,"テスト１"))
+#     classList.append(TankInterface(2,"テスト２"))
+
+#     for value in classList:
+#         print "===== class ====="
+#         print "code --> " + str(value.code)
+#         print "name --> " + value.name
